2c87fc116a329e50f89940e901b9a5e6374362ff
Rezaul-Karim-Sujon <44495035+Rezaul-Karim-Sujon@users.noreply.github.com>
2018-11-04 02:55:13 +0600

    Add files via upload

---
+++ Phone Diary/Phone Diary/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Phone_Diary
+{
+    public partial class PhoneBook : Form
+    {
+        string MyConnection2 = "datasource=localhost;username=root;password=;database=phone_book";
+        public PhoneBook()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PhoneBook_Load(object sender, EventArgs e)
+        {
+            this.ActiveControl = textBox1;
+            textBox1.Focus();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Clear();
+            textBox3.Text = "";
+            textBox4.Clear();
+
+            comboBox1.SelectedIndex = -1;
+
+            textBox1.Focus();	
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string MyConnection2 = "datasource=localhost;username=root;password=";
+                string Query = "select * from phone_book.users where first_name='" + this.searchBox5.Text + "' or last_name='" + this.searchBox5.Text + "' or mobile = '" + this.searchBox5.Text + "';";
+                MySqlConnection MyConn2 = new MySqlConnection(MyConnection2);
+                MySqlCommand MyCommand2 = new MySqlCommand(Query, MyConn2);
+
+                MySqlDataAdapter MyAdapter = new MySqlDataAdapter();
+                MyAdapter.SelectCommand = MyCommand2;
+                DataTable dTable = new DataTable();
+                MyAdapter.Fill(dTable);
+                dataGridView1.DataSource = dTable;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+               
+                
+                MySqlConnection MyConn2 = new MySqlConnection(MyConnection2);
+                MyConn2.Open();
+                string Query = "insert into users(first_name, last_name, mobile, email, category) values('" + this.textBox1.Text + "','" + this.textBox2.Text + "','" + this.textBox3.Text + "','" + this.textBox4.Text + "','" + this.comboBox1.Text + "');";
+
+                MySqlCommand MyCommand2 = new MySqlCommand(Query, MyConn2);
+                MySqlDataReader MyReader2;
+               
+
+                MyReader2 = MyCommand2.ExecuteReader();
+                MessageBox.Show("Account Inserted");
+                while (MyReader2.Read())
+                {
+                }
+                MyConn2.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string MyConnection2 = "datasource=localhost;username=root;password=;database=phone_book";
+                string Query = "delete from phone_book.users where mobile='" + this.textBox3.Text + "';";
+                MySqlConnection MyConn2 = new MySqlConnection(MyConnection2);
+                MySqlCommand MyCommand2 = new MySqlCommand(Query, MyConn2);
+                MySqlDataReader MyReader2;
+                MyConn2.Open();
+                MyReader2 = MyCommand2.ExecuteReader();
+                MessageBox.Show("Data Deleted");
+                while (MyReader2.Read())
+                {
+                }
+                MyConn2.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string MyConnection2 = "datasource=localhost;username=root;password=;database=phone_book";
+                //This is my update query in which i am taking input from the user through windows forms and update the record.  
+                string Query = "update phone_book.users set first_name='" + this.textBox1.Text + "',last_name='" + this.textBox2.Text + "',mobile='" + this.textBox3.Text + "',email='" + this.textBox4.Text + "',category='" + this.comboBox1.Text + "' where mobile='" + this.textBox3.Text + "';";
+                //This is  MySqlConnection here i have created the object and pass my connection string.  
+                MySqlConnection MyConn2 = new MySqlConnection(MyConnection2);
+                MySqlCommand MyCommand2 = new MySqlCommand(Query, MyConn2);
+                MySqlDataReader MyReader2;
+                MyConn2.Open();
+                MyReader2 = MyCommand2.ExecuteReader();
+                MessageBox.Show("Contact Updated!");
+                while (MyReader2.Read())
+                {
+                }
+                MyConn2.Close();//Connection closed here  
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Visible = true;
+            try
+            {
+                string MyConnection2 = "datasource=localhost;username=root;password=;database=phone_book";
+                string Query = "select * from users";
+                MySqlConnection MyConn2 = new MySqlConnection(MyConnection2);
+                MySqlCommand MyCommand2 = new MySqlCommand(Query, MyConn2);
+                //  MyConn2.Open();  
+                //For offline connection we weill use  MySqlDataAdapter class.  
+                MySqlDataAdapter MyAdapter = new MySqlDataAdapter();
+                MyAdapter.SelectCommand = MyCommand2;
+                DataTable dTable = new DataTable();
+                MyAdapter.Fill(dTable);
+                dataGridView1.DataSource = dTable; // here i have assign dTable object to the dataGridView1 object to display data.               
+                // MyConn2.Close();  
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void searchBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
